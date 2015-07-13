2d55f4753a48ea834715fe272251eb3588c63e6d
danny8508021 <dannywang0802@yahoo.com.tw>
2015-07-13 15:59:47 +0800

    merge

--- examples/server.js
+++ examples/server.js
@@ -3,13 +3,23 @@ var ws = require("./node_modules/nodejs-websocket")
 var fs = require("fs")
 var express = require( 'express' ) ;
 var app = express() ;
+var cors = require('cors');
 
 app.use(express.static('../'));
+app.use(cors());
 app.listen( 3000 ) ;
 
-http.createServer(function (req, res) {
-	fs.createReadStream("index.html").pipe(res)
-}).listen(8080)
+//// http.createServer(function (req, res) {
+//	res.setHeader('Access-Control-Allow-Origin', '*');
+//	res.setHeader('Access-Control-Request-Method', '*');
+//	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+//	res.setHeader('Access-Control-Allow-Headers', '*'); 
+//	fs.createReadStream("index.html").pipe(res);
+//}).listen(8080)
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {root: __dirname});
+});
 
 var server = ws.createServer(function (connection) {
 	connection.nickname = null
